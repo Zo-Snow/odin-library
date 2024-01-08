c317dfcff9bd2431b8e524027a0bd2bce481cd24
Zo-Snow <zcr.snow@gmail.com>
2024-01-08 15:54:14 +0300

    Make js functionality work

--- index.js
+++ index.js
@@ -1,18 +1,115 @@
-const myLibrary = [];
+const myLibrary = [
+    // {id: 0, title: 'MDZS', author: 'Mo Xiang Tong Xiu', pages: 396, status: 'Read'},  
+];
 
-function Book(title, author, pages, read) {
+const cards = document.querySelector(".cards");
+
+document.querySelector(".add-button").addEventListener('click', (event) => {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const checkStatus = document.querySelector("#status").checked;
+    let status = document.querySelector("#status").checked;
+
+    if (status === true) {
+        status = "Status: Read";
+    } else {
+        status = "Status: Not read yet";
+    }
+
+    let newId = myLibrary.length;
+
+    console.log(`${title}, ${author}, ${pages}, ${status}`);
+    let newBook = new Book(newId, title, author, pages, status);
+    myLibrary.push(newBook);
+    document.querySelector("form").reset();
+    displayBooks(checkStatus);
+    addToggleFunction(newId, title);
+    addDeleteFunction(newId, title);
+
+    event.preventDefault();
+})
+
+function Book(id, title, author, pages, status) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${pages} pages, ${this.read}`;
-    };
-
+    this.status = status;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-
-function addBookToLibrary() {
-    
+function addElementProperties(name, className, text, appendTo) {
+    name.classList = className;
+    name.textContent = text;
+    appendTo.appendChild(name);
 }
+
+function displayBooks(status) {
+
+    const card = document.createElement('div');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
+    const p4 = document.createElement('p');
+    const newInput = document.createElement('input');
+    const newButton = document.createElement('button');
+
+    myLibrary.forEach((book) => {
+
+        card.setAttribute("id", `card-${book.id}-${book.title}`);
+        
+        addElementProperties(card, "card", "", cards); 
+        addElementProperties(p1, "title", book.title, card);    
+        addElementProperties(p2, "author", book.author, card);     
+        addElementProperties(p3, "pages", book.pages, card);      
+        addElementProperties(p4, "status", book.status, card);
+        p4.setAttribute("id", `status-${book.id}-${book.title}`);
+
+        newInput.setAttribute("name", `status-toggle-${book.id}-${book.title}`);
+        newInput.setAttribute("type", "checkbox");
+        newInput.setAttribute("id", `status-toggle-${book.id}-${book.title}`);
+        newInput.setAttribute("aria-label", "toggle read status" ); 
+        newInput.setAttribute("class", "toggle-inputs" ); 
+        if (status === true) {
+            newInput.setAttribute("checked", true); 
+        } 
+        card.appendChild(newInput);
+
+        newButton.textContent = "Delete";
+        newButton.setAttribute("id", `delete-${book.id}-${book.title}`);
+        card.appendChild(newButton);
+    })
+}
+
+function addToggleFunction(id, title) {
+    const toggle = document.querySelector(`#status-toggle-${id}-${title}`);
+
+    let currentToggle = toggle.id.slice(14).split("-");
+    let neededId = currentToggle[0];
+    let neededTitle = currentToggle[1];
+    let currentPara = document.querySelector(`#status-${neededId}-${neededTitle}`);
+
+    toggle.addEventListener('click', () => {
+    if (toggle.checked === true) {
+        currentPara.textContent = "Status: Read";
+    } else {
+        currentPara.textContent = "Status: Not read yet.";
+    }})}
+
+function addDeleteFunction(id, title) {
+    const button = document.querySelector(`#delete-${id}-${title}`);
+    button.addEventListener('click', (event) => { 
+
+        let currentCard = document.querySelector(`#card-${id}-${title}`);
+        currentCard.remove();
+
+        myLibrary.forEach((book) => {
+            if (book.id === id && book.title === title) {
+                const index = myLibrary.indexOf(book);
+                const x = myLibrary.splice(index, 1);
+            }
+        });
+        event.preventDefault();
+    })
+}
+
